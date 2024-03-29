de7b8bf4967135c23e79bb775bfa5567ba0e00d9
Donjerzy <okoth46@gmail.com>
2024-03-29 11:45:48 +0300

    Checkin Construction Management Banner

--- back-end/src/main/kotlin/com/construction/management/cm/CmApplication.kt
+++ back-end/src/main/kotlin/com/construction/management/cm/CmApplication.kt
@@ -1,6 +1,5 @@
 package com.construction.management.cm
 
-import io.github.cdimascio.dotenv.Dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -8,6 +7,5 @@ import org.springframework.boot.runApplication
 class CmApplication
 
 fun main(args: Array<String>) {
-	val dotenv: Dotenv = Dotenv.load()
 	runApplication<CmApplication>(*args)
 }
