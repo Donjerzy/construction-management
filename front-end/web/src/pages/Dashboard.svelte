084facc3f2f120fd5ca3130fbf7476f8387d97d0
Donjerzy <okoth46@gmail.com>
2024-04-19 18:00:25 +0300

    Add admin layout

--- front-end/web/src/pages/Dashboard.svelte
+++ front-end/web/src/pages/Dashboard.svelte
@@ -1,9 +1,10 @@
 <script>
     import {firstName} from '../stores.js' 
     import { get } from "svelte/store";
+    import AdminComponent from '../components/admin-component.svelte';
     let userFirstName = get(firstName);
+    let appName = 'Mjengo Bora Construction'
 </script>
-
 
 
 <svelte:head>
@@ -11,27 +12,4 @@
 </svelte:head>
 
 
-<div class="container">
-    <h1>Hello {userFirstName}</h1>
-</div>
-
-
-
-<style>
-    :global(body) {
-        margin: 0; 
-        padding: 0; 
-        box-sizing: border-box;
-        max-width: 100vw;
-        min-height: 100vh;
-    }
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
-    .container {
-        min-height: 100vh;
-        width: 100%;
-    }
-</style>
+<AdminComponent appName={appName} userFirstName={userFirstName}></AdminComponent>
