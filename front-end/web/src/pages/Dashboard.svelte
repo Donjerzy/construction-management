52293618a70859165713c913c9f1c188f7c9a14c
Donjerzy <okoth46@gmail.com>
2024-04-19 16:35:26 +0300

    Add admin dashboard design

--- front-end/web/src/pages/Dashboard.svelte
+++ front-end/web/src/pages/Dashboard.svelte
@@ -6,4 +6,32 @@
 
 
 
-<h1>Hello {userFirstName}</h1>
+<svelte:head>
+    <title>Dashboard</title>
+</svelte:head>
+
+
+<div class="container">
+    <h1>Hello {userFirstName}</h1>
+</div>
+
+
+
+<style>
+    :global(body) {
+        margin: 0; 
+        padding: 0; 
+        box-sizing: border-box;
+        max-width: 100vw;
+        min-height: 100vh;
+    }
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    .container {
+        min-height: 100vh;
+        width: 100%;
+    }
+</style>
