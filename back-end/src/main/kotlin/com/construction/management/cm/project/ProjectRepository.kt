69e6b9416487d94b3d1ba1d39cf1acfae4960f42
Donjerzy <okoth46@gmail.com>
2024-04-19 10:41:40 +0300

    Add project_id to create project

--- back-end/src/main/kotlin/com/construction/management/cm/project/ProjectRepository.kt
+++ back-end/src/main/kotlin/com/construction/management/cm/project/ProjectRepository.kt
@@ -2,12 +2,16 @@ package com.construction.management.cm.project
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import java.util.UUID
 
 interface ProjectRepository: JpaRepository<Project, Long> {
 
 
     @Query("select count(*) from project where lower(name) = :project and project_manager = :user", nativeQuery = true)
     fun projectExists(project:String, user:Long): Int
+
+    @Query("select count(*) from project where project_id = :uuid", nativeQuery = true)
+    fun projectUuidExists(uuid: UUID): Int
 
 
 }
