cec605739de0fadb33efe7a8de184a7f67812b54
Donjerzy <okoth46@gmail.com>
2024-05-23 17:25:48 +0300

    Add employee morale front end

--- front-end/web/src/pages/IndividualEmployee.svelte
+++ front-end/web/src/pages/IndividualEmployee.svelte
@@ -23,7 +23,7 @@
         "tasksOngoing": 0,
         "wage": "20000",
         "wageType": "Monthly",
-        "archetype": "n/a",
+        "morale": "n/a",
         "totalTasks": 0,
         "joinDate": "2024-02-25",
         "hasContract": "yess",
@@ -354,15 +354,19 @@
 
         <div class="flex gap-7 border border-primary-100 rounded-md w-fit p-4 bg-white shadow-xl">
             <div>
-                <svg class="w-10 h-10 mx-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M13 3C9.23 3 6.19 5.95 6 9.66L4.08 12.19C3.84 12.5 4.08 13 4.5 13H6V16C6 17.11 6.89 18 8 18H9V21H16V16.31C18.37 15.19 20 12.8 20 10C20 6.14 16.88 3 13 3M17.06 9.57L15.1 10.09L16.54 11.54C16.89 11.88 16.89 12.46 16.54 12.81C16.19 13.16 15.61 13.16 15.27 12.81L13.81 11.37L13.3 13.33C13.18 13.82 12.68 14.1 12.21 13.97C11.72 13.84 11.44 13.35 11.57 12.87L12.1 10.9L10.13 11.43C9.65 11.56 9.15 11.28 9.03 10.79C8.9 10.32 9.18 9.82 9.67 9.7L11.63 9.19L10.19 7.73C9.84 7.39 9.84 6.82 10.19 6.46C10.54 6.11 11.12 6.11 11.46 6.46L12.91 7.9L13.43 5.94C13.55 5.46 14.04 5.18 14.5 5.3C15 5.43 15.28 5.92 15.16 6.41L14.63 8.37L16.59 7.84C17.08 7.72 17.57 8 17.7 8.5C17.82 8.96 17.54 9.45 17.06 9.57Z" /></svg>
+                <svg class="w-10 h-10 mx-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M17.66 11.2C17.43 10.9 17.15 10.64 16.89 10.38C16.22 9.78 15.46 9.35 14.82 8.72C13.33 7.26 13 4.85 13.95 3C13 3.23 12.17 3.75 11.46 4.32C8.87 6.4 7.85 10.07 9.07 13.22C9.11 13.32 9.15 13.42 9.15 13.55C9.15 13.77 9 13.97 8.8 14.05C8.57 14.15 8.33 14.09 8.14 13.93C8.08 13.88 8.04 13.83 8 13.76C6.87 12.33 6.69 10.28 7.45 8.64C5.78 10 4.87 12.3 5 14.47C5.06 14.97 5.12 15.47 5.29 15.97C5.43 16.57 5.7 17.17 6 17.7C7.08 19.43 8.95 20.67 10.96 20.92C13.1 21.19 15.39 20.8 17.03 19.32C18.86 17.66 19.5 15 18.56 12.72L18.43 12.46C18.22 12 17.66 11.2 17.66 11.2M14.5 17.5C14.22 17.74 13.76 18 13.4 18.1C12.28 18.5 11.16 17.94 10.5 17.28C11.69 17 12.4 16.12 12.61 15.23C12.78 14.43 12.46 13.77 12.33 13C12.21 12.26 12.23 11.63 12.5 10.94C12.69 11.32 12.89 11.7 13.13 12C13.9 13 15.11 13.44 15.37 14.8C15.41 14.94 15.43 15.08 15.43 15.23C15.46 16.05 15.1 16.95 14.5 17.5H14.5Z" /></svg>
             </div>
             <div class="flex flex-col">
                 <p class="underline text-lg">Other Information</p>
                 <div class="mt-2">
                     <div class="flex flex-col gap-1 max-h-72 overflow-auto w-[340px]">
-                        <div class="flex items-center justify-between pr-4 border-b pb-2">
-                            <p class="text-base">Archetype:</p>
-                            <p class="italic text-sm">{employee.archetype}</p>
+                        <div class="flex items-center justify-between pr-4 pb-10 ">
+                            <div class="tooltip">
+                               <p class="font-mono text-base underline hover:cursor-pointer">Morale: ?</p> 
+                                <span class="tooltiptext">Based on employee comments</span>
+                            </div>
+                            
+                            <p class="italic text-sm">{employee.morale}</p>
                         </div>  
                     </div>
                 </div>  
@@ -467,3 +471,36 @@
         </div>
     </div>
 </AdminComponent>
+
+
+<style>
+
+.tooltip {
+  position: relative;
+  display: inline-block;
+  cursor: default;
+}
+
+.tooltip .tooltiptext {
+  visibility: hidden;
+  padding: 0.25em 0.5em;
+  background-color: black;
+  color: #fff;
+  text-align: center;
+  border-radius: 0.25em;
+  white-space: nowrap;
+  
+  /* Position the tooltip */
+  position: absolute;
+  z-index: 1;
+  top: 100%;
+  left: 100%;
+  transition-property: visibility;
+  transition-delay: 0s;
+}
+
+.tooltip:hover .tooltiptext {
+  visibility: visible;
+  transition-delay: 0.3s;
+}
+</style>
--- front-end/web/vite.config.js
+++ front-end/web/vite.config.js
@@ -1,6 +1,9 @@
+
 import { sveltekit } from '@sveltejs/kit/vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
+
+
 
 export default defineConfig({
 	plugins: [sveltekit()]
