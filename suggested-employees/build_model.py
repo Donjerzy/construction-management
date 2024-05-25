a09528a4c0fce8a2556bb531c5220e234ec8715c
Donjerzy <okoth46@gmail.com>
2024-05-25 08:29:12 +0300

    Connect to db

--- suggested-employees/build_model.py
+++ suggested-employees/build_model.py
@@ -1,12 +1,28 @@
 from dotenv import load_dotenv
 import os
+from sqlalchemy import create_engine
+import psycopg2
 
+
+def connect_to_db():
+    user = os.getenv('USER')
+    password = os.getenv('PASS')
+    host = os.getenv('HOST')
+    port = os.getenv('PORT')
+    db_name = os.getenv('DB_NAME')
+    postgres_db_conn_str = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{db_name}'
+    engine = create_engine(postgres_db_conn_str)
+    try:
+        connection = engine.connect()
+        connection.close()
+        return engine
+    except Exception as e:
+        print("Could not connect to construction management db")
+        quit()
 
 def main():
     load_dotenv()
-    test = os.getenv("Test")
-    print(f"{test}")
-
+    engine = connect_to_db()
 
 if __name__ == '__main__':
     main()
